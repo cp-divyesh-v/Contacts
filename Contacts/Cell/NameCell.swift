014f6323133a3646b6512ef9ad380a41c8d5d49c
divyesh <divyesh.v@canopas.com>
2021-09-22 18:24:55 +0530

    change in home controller and cell

--- Contacts/Cell/NameCell.swift
+++ Contacts/Cell/NameCell.swift
@@ -2,16 +2,24 @@
 //  NameCell.swift
 //  Contacts
 //
-//  Created by Divyesh Vekariya on 16/09/21.
+//  Created by Divyesh Vekariya on 22/09/21.
 //
 
 import UIKit
 
-class NameCell: UICollectionViewCell {
+class NameCell: UITableViewCell {
 
+    @IBOutlet weak var carecterView: UIView!
+    @IBOutlet weak var charecterLabel: UILabel!
+    @IBOutlet weak var nameLabel: UILabel!
+    
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
     }
 
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+    }
+    
 }
